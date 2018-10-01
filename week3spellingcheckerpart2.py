a13a31a3a6618b0133edaff4d41b9cb118598a85
watsap147 <43532623+watsap147@users.noreply.github.com>
2018-10-01 12:17:46 +0200

    Add files via upload

---
+++ week3spellingcheckerpart2.py
@@ -0,0 +1,16 @@
+wrongwords = ["kut", "gvd", "nee", "ja", "omg", "facebook", "arjen"]
+goeieworden = ["jammer", "helaas", "misschien", "wel", "umg", "twitter", "naam"]
+zin = input("schrijf eens een leuke zin")
+
+index = 0
+woorden = zin.split()
+for woord in woorden:
+	if woord in wrongwords:
+		goeie_woord_index = wrongwords.index(woord)
+		print(goeie_woord_index)
+		woorden[index] = goeieworden[goeie_woord_index]
+		# zinlijst.index(wrondwords[index]) = goeieworden[index]
+	index = index + 1
+woorden = " ".join(woorden)
+print(woorden)
+
