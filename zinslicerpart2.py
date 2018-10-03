02337623cfe2dc9f2e6812baa45500d378c6b4b1
watsap147 <43532623+watsap147@users.noreply.github.com>
2018-10-03 10:46:06 +0200

    Add files via upload

---
+++ zinslicerpart2.py
@@ -0,0 +1,7 @@
+sentence = input("What is your sentence? ")
+sentence_list = sentence.split()
+sentence_listlen = len(sentence_list)
+sentence_25 = round(sentence_listlen * 0.25)
+sentence_75 = round(sentence_listlen * 0.75)
+print(sentence_list[sentence_25 : sentence_75])
+
