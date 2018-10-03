a83ddcbb5550641e008ea9273600a5e848da5b63
watsap147 <43532623+watsap147@users.noreply.github.com>
2018-10-03 10:54:40 +0200

    Update zinslicerpart2.py

--- zinslicerpart2.py
+++ zinslicerpart2.py
@@ -3,5 +3,5 @@ sentence_list = sentence.split()
 sentence_listlen = len(sentence_list)
 sentence_25 = round(sentence_listlen * 0.25)
 sentence_75 = round(sentence_listlen * 0.75)
-print(sentence_list[sentence_25 : sentence_75])
-
+sentence_percentage = sentence_list[sentence_25 : sentence_75]
+print(" ".join(sentence_percentage))
